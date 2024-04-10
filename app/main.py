52f4aa4434baefb00723142dacc423623ec17af6
jaymanvirk <jay.manvirk@gmail.com>
2024-04-10 16:19:12 +0700

    added sign_in_token; added get_user_list function

--- app/main.py
+++ app/main.py
@@ -2,36 +2,42 @@ from fastapi import FastAPI, Request
 from fastapi.staticfiles import StaticFiles
 from pydantic import BaseModel
 from motor.motor_asyncio import AsyncIOMotorClient
+import secrets
+
 
 app = FastAPI()
 
 
 # MongoDB setup
-client = AsyncIOMotorClient("mongodb://db")
+client = AsyncIOMotorClient("mongodb://docker_mongodb")
 db = client.user_profiles
 
 class UserProfile(BaseModel):
     email: str
-
+    sign_in_token: str
 
 @app.post("/signin")
 async def sign_in(request: Request):
-    # Parse the JSON body of the request
+    # Get user data in JSON
     data = await request.json()
+    sign_in_token = get_sign_in_token()
+    data["sign_in_token"] = sign_in_token
     user = UserProfile(**data)
 
     # Insert the user into MongoDB
     result = await db.users.insert_one(user.dict())
-    return {"_id": str(result.inserted_id)}
+    return {"sign_in_token": str(sign_in_token)}
 
-# @app.get("/user/{user_id}")
-# async def read_user(user_id: str):
-#     user = await db.users.find_one({"_id": user_id})
-#     if user:
-#         return user
-#     else:
-#         return {"error": "User not found"}
+def get_sign_in_token():
+    # Generate a secure random token
+    token = secrets.token_urlsafe(32) 
+    return token
 
 
+@app.get("/get_user_list")
+async def get_user_list():
+    data = await db.users.find({}, {"_id": False}).to_list(length=None)
+
+    return {"data": str(data)}
 
 app.mount("/", StaticFiles(directory="static", html = True), name="static")
