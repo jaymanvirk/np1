70dfe6cdf0fb1bbfbd494d53f2c6747c2e3c9a57
jaymanvirk <jay.manvirk@gmail.com>
2024-04-11 16:56:04 +0700

    introduced send_magic_link() and verify_magic_link() along with helper functions; added clear_collection() to refresh collection

--- app/main.py
+++ app/main.py
@@ -1,43 +1,119 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, HTTPException
 from fastapi.staticfiles import StaticFiles
 from pydantic import BaseModel
 from motor.motor_asyncio import AsyncIOMotorClient
-import secrets
-
+import uuid
+from datetime import datetime, timedelta
 
 app = FastAPI()
 
 
 # MongoDB setup
 client = AsyncIOMotorClient("mongodb://docker_mongodb")
-db = client.user_profiles
 
-class UserProfile(BaseModel):
+class UserSession(BaseModel):
     email: str
-    sign_in_token: str
+    sign_in_token: str = None
+    sign_in_token_created_at: float = None
 
-@app.post("/signin")
-async def sign_in(request: Request):
-    # Get user data in JSON
+@app.post("/send_magic_link")
+async def send_magic_link(request: Request):
     data = await request.json()
-    sign_in_token = get_sign_in_token()
-    data["sign_in_token"] = sign_in_token
-    user = UserProfile(**data)
+    sign_in_token = await generate_sign_in_token(data.get("email"))
 
-    # Insert the user into MongoDB
-    result = await db.users.insert_one(user.dict())
-    return {"sign_in_token": str(sign_in_token)}
+    magic_link = f"http://0.0.0.0:8000/verify_magic_link?sign_in_token={sign_in_token}"
+
+    return str(magic_link)
+
+async def generate_sign_in_token(email):
+    us = get_user_session(email)
+    db = client["users"]
+    collection = db["sessions"]
+
+    user_exist = await collection.find_one({"email": us.email})
+    if not user_exist:
+        await collection.insert_one(us.dict())
+    else:
+        await collection.update_one(
+            {
+                "email": us.email
+            }
+            , {
+                "$set": {
+                            "sign_in_token": us.sign_in_token
+                            , "sign_in_token_created_at": us.sign_in_token_created_at
+                        }
+                }
+            )
+
+
+    return us.sign_in_token
+
+def get_user_session(email):
+    user_session = UserSession(
+        email = email
+        , sign_in_token = get_sign_in_token()
+        , sign_in_token_created_at = get_sign_in_token_created_at()
+    )
+
+    return user_session
 
 def get_sign_in_token():
-    # Generate a secure random token
-    token = secrets.token_urlsafe(32) 
-    return token
+    return str(uuid.uuid4())
+
+def get_sign_in_token_created_at():
+    return datetime.utcnow().timestamp()
+
+
+@app.get("/verify_magic_link")
+async def verify_magic_link(sign_in_token: str):
+    db = client["users"]
+    collection = db["sessions"]
+
+    us = await collection.find_one({"sign_in_token": sign_in_token})
+    if not us:
+        raise HTTPException(status_code=404, detail="Invalid magic link")
+    else:
+        us = UserSession(**us)
+        ts = datetime.fromtimestamp(us.sign_in_token_created_at)
+        token_age = datetime.utcnow() - ts
+        if token_age > timedelta(minutes=1):
+            raise HTTPException(status_code=401, detail="Token expired")
+
+    await collection.update_one(
+        {
+            "email": us.email
+        }
+        , {
+            "$unset": {
+                        "sign_in_token": None
+                        , "sign_in_token_created_at": None
+                        }
+            }
+        )
+    
+
+    return {"User authenticated"}
 
 
 @app.get("/get_user_list")
 async def get_user_list():
-    data = await db.users.find({}, {"_id": False}).to_list(length=None)
+    db = client["users"]
+    collection = db["sessions"]
+
+    data = await collection.find({}, {"_id": 0}).to_list(length=None)
 
     return {"data": str(data)}
+
+
+@app.get("/clear_collection")
+async def clear_collection():
+    db = client["users"]
+    collection = db["sessions"]
+
+    result = await collection.delete_many({})
+
+    return {"documents were deleted"}
+
 
 app.mount("/", StaticFiles(directory="static", html = True), name="static")
