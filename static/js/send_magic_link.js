58ae76887ae2b6fce4d651a9504826882494cd84
jaymanvirk <jay.manvirk@gmail.com>
2024-04-17 22:37:21 +0700

    updated API endpoint

--- static/js/send_magic_link.js
+++ static/js/send_magic_link.js
@@ -1,7 +1,7 @@
 async function send_magic_link(){
     const email =  document.getElementById('email').value;
 
-    const response = await fetch("/user_auth/send_magic_link", {
+    const response = await fetch("/user_management/send_magic_link", {
         method: "POST",
         headers: {
             "Content-Type": "application/json"
