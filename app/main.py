99def592230c9568374e5888b2a26c3b0b924652
jaymanvirk <jay.manvirk@gmail.com>
2024-04-17 22:33:12 +0700

    switched to use separate user_router script

--- app/main.py
+++ app/main.py
@@ -1,11 +1,9 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
-from user_auth import router as user_auth_router
-from user_management import router as user_management_router
+from user_router import router as user_router
 
 app = FastAPI()
 
-app.include_router(user_auth_router, prefix="/user_auth")
-app.include_router(user_management_router, prefix="/user_management")
+app.include_router(user_router, prefix="/user_management")
 
 app.mount("/", StaticFiles(directory="static", html = True), name="static")
