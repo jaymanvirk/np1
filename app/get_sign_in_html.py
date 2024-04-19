6f8b94001bf45b08e30b66256b2326265e30911f
jaymanvirk <jay.manvirk@gmail.com>
2024-04-19 13:28:59 +0700

    moved to html_content folder

--- app/get_sign_in_html.py
+++
@@ -1,8 +0,0 @@
-def get_sign_in_html() -> str:
-	html = '''
-			<h1>Sign In</h1>
-			<input type="email" id="email" placeholder="Email" required>
-			<button id="send_magic_link">Send Magic Link</button>
-			'''
-			
-	return html
