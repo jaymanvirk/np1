47850f8d9a736132b267fda445ffe7defcaed9a3
jaymanvirk <jay.manvirk@gmail.com>
2024-04-21 15:59:14 +0700

    incomplete full class refactoring

--- app/user_management.py
+++ app/user_management.py
@@ -8,50 +8,91 @@ class UserManagement(MongoDBService):
                             , "auths": self.db["auths"]
                             , "sessions": self.db["sessions"]
                             }
-
-    def set_user_id(self, user_id):
-        self.user_id = user_id
+        self.ids = {}
 
 
-    async def get_insert_user_id(self, email: str):
-        email_dict = {"email": email}
-        user_profile = await self.collections["profiles"].find_one(email_dict)
-
-        if not user_profile:
-            result = await self.collections["profiles"].insert_one(email_dict)
-            user_profile = {}
-            user_profile["_id"] = result.inserted_id
-            await self.collections["auths"].insert_one({"user_id": user_profile["_id"]})
-
-        self.set_user_id(user_profile["_id"])
-
-        return user_profile["_id"]
+    async def get_one_document(self, collection_name: str, _filter):
+        return await self.collections[collection_name].find_one(_filter)
 
 
-    async def get_update_auth_token(self, operator = "set"):
-        auth_dict = get_token()
+    async def set_one_document(self, data: dict, collection_name: str, _filter, operator = "set"):
+        collection = self.collections[collection_name]
 
-        await self.collections["auths"].update_one(
-            {"user_id": self.user_id}
-            , {
-                f"${operator}": auth_dict
-            }
-        )
+        result = await self.get_one_document(collection_name = collection_name
+                                                    , _filter = _filter)
+        if not result:
+            tmp = await collection.insert_one(data)
+            result = {}
+            result["_id"] = tmp.inserted_id
+        else:
+            await collection.update_one(
+                _filter
+                , {
+                    f"${operator}": data
+                }
+            )
 
-        return auth_dict["token"]
+        self.ids[collection_name] = result["_id"]
+
+
+    async def set_user_profile(self, data: dict):
+        await self.set_one_document(data = data
+                            , collection_name = "profiles"
+                            , _filter = {"email": data["email"]}
+                            )
+
+    async def set_user_auth(self, data: dict):
+        await self.set_one_document(data = data
+                            , collection_name = "auths"
+                            , _filter = {"token": data["token"]}
+                            )
+
+    def set_user_sign_in(self, data: dict):
+        args = [{}]
+
+        [await self.set_one_document(d) for d in args]
 
 
     async def set_user_session(self, data: dict) -> None:
-        user_id_dict = {"user_id": self.user_id}
-        user_session = await self.collections["sessions"].find_one(user_id_dict)
+        collection_name = "sessions"
+        collection = self.collections[collection_name]
+        _filter = {"token": data["token"]}
 
+        user_session = await self.get_one_document(collection_name = collection_name
+                                                    , _filter = _filter)
         if not user_session:
-            await self.collections["sessions"].insert_one({**user_id_dict, **data})
+            await collection.insert_one(data)
         else:
-            await self.collections["sessions"].update_one(
-                user_id_dict
+            await collection.update_one(
+                _filter
                 , {"$set": data}
             )
+
+
+    async def get_user_profile(self, session_token):
+        collection_name = "sessions"
+        collection = self.collections["sessions"]
+
+        pipeline = [
+            {"$match": {"token": session_token}}
+            , {
+                "$lookup": {
+                    "from": "profiles"
+                    , "localField": "user_id"
+                    , "foreignField": "_id"
+                    , "as": "user_profile"
+                }
+            }
+            , {
+                "$project": {
+                    "_id": 0
+                }
+            }
+        ]
+
+        user_profile = await collection.aggregate(pipeline)
+
+        return user_profile
 
 
 
