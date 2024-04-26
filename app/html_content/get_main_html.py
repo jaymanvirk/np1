f7a0d25957109ee3a508066b4de118754bdaa8d2
jaymanvirk <jay.manvirk@gmail.com>
2024-04-26 16:20:35 +0700

    updated html content

--- app/html_content/get_main_html.py
+++ app/html_content/get_main_html.py
@@ -1,4 +1,7 @@
 def get_main_html(data):
-	html = data
+	html = '''
+			<button id="start_streaming">Start Streaming</button>
+    		<button id="stop_streaming">Stop Streaming</button>
+    		'''
 
 	return html
