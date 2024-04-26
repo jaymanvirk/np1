76b75101c61504bab22f932351931a52a51f461c
jaymanvirk <jay.manvirk@gmail.com>
2024-04-26 16:20:52 +0700

    added websocket router

--- app/main.py
+++ app/main.py
@@ -1,11 +1,13 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
-from user_router import router as user_router
+from user_management_router import router as um_router
+from websocket_router import router as ws_router
 from fastapi.middleware import Middleware
 from sanitize_middleware import SanitizeMiddleware
 
 app = FastAPI() # FastAPI(middleware=[Middleware(SanitizeMiddleware)])
 
-app.include_router(user_router, prefix="/user_management")
+app.include_router(um_router, prefix="/user_management")
+app.include_router(ws_router)
 
 app.mount("/", StaticFiles(directory="static", html = True), name="static")
