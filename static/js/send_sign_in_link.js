db04d00357276511bc72bc25a4fddbf2279759ce
jaymanvirk <jay.manvirk@gmail.com>
2024-04-27 16:47:23 +0700

    changed name

---
+++ static/js/send_sign_in_link.js
@@ -0,0 +1,18 @@
+async function send_magic_link(){
+    const email =  document.getElementById('email').value;
+
+    const response = await fetch("/auth/send_sign_in_link", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({"email":email})
+    });
+
+    if (response.ok) {
+        const data = await response.json();
+        console.log(data);
+    } else {
+        console.error(response.statusText);
+    }
+}
