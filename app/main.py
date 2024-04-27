54fb9aa4890115d0b924e5a920e7de89fd5d338c
jaymanvirk <jay.manvirk@gmail.com>
2024-04-27 16:45:15 +0700

    changed user_management to auth

--- app/main.py
+++ app/main.py
@@ -1,13 +1,13 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
-from user_management_router import router as um_router
+from auth_router import router as auth_router
 from websocket_router import router as ws_router
 from fastapi.middleware import Middleware
 from sanitize_middleware import SanitizeMiddleware
 
 app = FastAPI() # FastAPI(middleware=[Middleware(SanitizeMiddleware)])
 
-app.include_router(um_router, prefix="/user_management")
+app.include_router(auth_router, prefix="/auth")
 app.include_router(ws_router)
 
 app.mount("/", StaticFiles(directory="static", html = True), name="static")
