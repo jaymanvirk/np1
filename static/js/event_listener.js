fb80fe63c14146c9a8367a53876f17fc1f011cd3
jaymanvirk <jay.manvirk@gmail.com>
2024-06-05 22:22:36 +0700

    added if condition on null element

--- static/js/event_listener.js
+++ static/js/event_listener.js
@@ -2,13 +2,19 @@ document.addEventListener('DOMContentLoaded', function() {
 
 
     const button_sign_in_link = document.getElementById('send_sign_in_link');
-    button_sign_in_link.addEventListener('click', send_sign_in_link);
+    if (button_sign_in_link){
+        button_sign_in_link.addEventListener('click', send_sign_in_link);
+    }
 
     const zone_drag_drop_image = document.getElementById('drop_image');
-    zone_drag_drop_image.addEventListener('dragover', drag_image);
-    zone_drag_drop_image.addEventListener('drop', drop_image);
+    if (zone_drag_drop_image){
+        zone_drag_drop_image.addEventListener('dragover', drag_image);
+        zone_drag_drop_image.addEventListener('drop', drop_image);
+    }
 
     const button_select_image = document.getElementById('select_image');
-    button_select_image.addEventListener('change', handle_select_image);
+    if (button_select_image){
+        button_select_image.addEventListener('change', handle_select_image);
+    }
 
 });
