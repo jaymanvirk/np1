a349a5994446e283b534c5e4f6c40c8ec8afc6ca
jaymanvirk <jay.manvirk@gmail.com>
2024-08-29 14:46:29 +0300

    disabled sanitize_middleware

--- app/main.py
+++ app/main.py
@@ -3,7 +3,7 @@ from fastapi.staticfiles import StaticFiles
 from auth_router import router as auth_router
 from websocket_router import router as ws_router
 from fastapi.middleware import Middleware
-from sanitize_middleware import SanitizeMiddleware
+# from sanitize_middleware import SanitizeMiddleware
 
 app = FastAPI() # FastAPI(middleware=[Middleware(SanitizeMiddleware)])
 
