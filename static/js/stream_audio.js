e6990e3d7daa9766300e5edae95eac61cd84d83f
jaymanvirk <jay.manvirk@gmail.com>
2024-08-29 14:49:09 +0300

    set interval to 100 ms

--- static/js/stream_audio.js
+++ static/js/stream_audio.js
@@ -1,32 +1,32 @@
-const ws = new WebSocket("ws://localhost:8000/stream/audio");
-
 async function stream_audio(){
-
-    ws.onopen = async () => {
-	    console.log("WebSocket connection established");
-
-	    const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
-	    const mediaRecorder = new MediaRecorder(stream);
-
-	    mediaRecorder.ondataavailable = event => {
-	        if (event.data.size > 0) {
-	            ws.send(event.data);
-	        }
-	    };
-
-	    mediaRecorder.start(25); // Send audio data every 25 milliseconds
+	const ws = new WebSocket("ws://localhost:8000/stream/audio");
+	ws.onopen = async () => {
+		console.log("WebSocket connection established");
 	};
 
-   	ws.onmessage = (event) => {
-  		const transcriptionDiv = document.getElementById('chat_log');
-        transcriptionDiv.innerHTML += event.data;
-    };
+	ws.onmessage = (event) => {
+		const transcriptionDiv = document.getElementById('chat_log');
+	    transcriptionDiv.innerHTML += event.data;
+	};
 
-    ws.onclose = () => {
-      console.log('WebSocket connection closed');
-    };
+	ws.onclose = () => {
+		console.log('WebSocket connection closed');
+	};
 
-    ws.onerror = (event) => {
-      console.log('WebSocket error:', event);
-    };
+	ws.onerror = (event) => {
+		console.log('WebSocket error:', event);
+	};
+	let mediaRecorder;
+    navigator.mediaDevices.getUserMedia({ audio: true })
+	.then(stream => {
+        mediaRecorder = new MediaRecorder(stream);
+
+        mediaRecorder.ondataavailable = event => {
+            if (event.data.size > 0) {
+                ws.send(event.data);
+            }
+        };
+        mediaRecorder.start(100);
+    })
+    .catch(error => console.error("Error accessing microphone: ", error));
 };
