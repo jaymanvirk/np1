843e68be2adee4f5b37540c01936d6aaa0cb99b9
jaymanvirk <jay.manvirk@gmail.com>
2024-08-30 16:08:51 +0300

    for loop with segments doesn't return segments; returns only list(segments)

--- app/websocket_router.py
+++ app/websocket_router.py
@@ -25,33 +25,38 @@ async def handle_stream_audio(websocket: WebSocket):
     await websocket.accept()
     try:
         while True:
-            print("ENTERED THE LOOP")
             try:
                 message = await websocket.receive_bytes()
-                print(len(message))
+                await websocket.send_text(f"1. completed websocket.receive_bytes: {len(message)}")
             except Exception as e:
-                print(f"Error receiving message: {e}")
+                await websocket.send_text(f"1. Error receiving message: {e}")
                 break
+
+            while len(message) % 2 != 0:
+                message += b'\x00'
                 
-            # try:
-            #     audio_data = np.frombuffer(message, dtype=np.int16)
-            # except Exception as e:
-            #     print(f"Error np.frombuffer: {e}")
-            #     break
+            try:
+                audio_data = np.frombuffer(message, dtype=np.int16)
+                await websocket.send_text(f"2.completed np.frombuffer: {len(audio_data)}")
+            except Exception as e:
+                await websocket.send_text(f"2. Error np.frombuffer: {e}")
+                break
 
-            # try:
-            #     audio_data_float = audio_data.astype(np.float32) / 32768.0
-            # except Exception as e:
-            #     print(f"Error .astype: {e}")
-            #     break
+            try:
+                audio_data_float = audio_data.astype(np.float32) / 32768.0
+                await websocket.send_text(f"3. completed audio_data.astype: {len(audio_data_float)}")
+            except Exception as e:
+                await websocket.send_text(f"3. Error audio_data.astype: {e}")
+                break
 
-            # try:
-            #     segments, _ = get_transcription(audio_data_float)
-            #     for segment in segments:
-            #         await websocket.send_text(segment.text)
-            # except Exception as e:
-            #     print(f"Error get_transcription: {e}")
-            #     break
+            try:
+                segments, info = get_transcription(audio_data_float)
+                await websocket.send_text(f"4. completed get_transcription: {list(segments)}")
+                for segment in segments:
+                    await websocket.send_text(f"{segment.start}, {segment.end}, {segment.text}")
+            except Exception as e:
+                await websocket.send_text(f"4. Error get_transcription: {e}")
+                break
     except Exception as e:
         print(f"Error: {e}")
     finally:
