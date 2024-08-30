eb8b7033a4949c437053abd5beae6fadc68125d9
jaymanvirk <jay.manvirk@gmail.com>
2024-08-30 16:07:48 +0300

    added test on file

--- app/get_transcription.py
+++ app/get_transcription.py
@@ -1,14 +1,16 @@
 from faster_whisper import WhisperModel
 
 def get_transcription(audio_data_float):
-    model_size = "base"
-
+    model_size = "tiny.en"
+    device = "cpu"
+    compute_type = "int8"
+    beam_size = 1
+    
     # # Run on GPU with FP16
     # model = WhisperModel(model_size, device="cuda", compute_type="float16")
     # # or run on GPU with INT8
     # model = WhisperModel(model_size, device="cuda", compute_type="int8_float16")
-    # # or run on CPU with INT8
-    # model = WhisperModel(model_size, device="cpu", compute_type="int8")
-
-
-    return ([{'text':"test message"}],1) # model.transcribe(audio_data_float, beam_size=5)
+    # or run on CPU with INT8
+    model = WhisperModel(model_size, device=device, compute_type=compute_type)
+    
+    return model.transcribe("test.m4a", beam_size=beam_size)
