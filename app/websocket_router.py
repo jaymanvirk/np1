bf0d5d5c9910a64437083019670a25ffb9dab8d3
jaymanvirk <jay.manvirk@gmail.com>
2024-09-02 14:16:01 +0300

    tested file-like io.BytesIO() creation to pass to get_transcription(); works one time then throws an invalid data error

--- app/websocket_router.py
+++ app/websocket_router.py
@@ -1,6 +1,7 @@
 from fastapi import WebSocket, WebSocketDisconnect, APIRouter
 from get_transcription import get_transcription
 import numpy as np
+import io
 
 router = APIRouter()
 
@@ -32,31 +33,34 @@ async def handle_stream_audio(websocket: WebSocket):
                 await websocket.send_text(f"1. Error receiving message: {e}")
                 break
 
-            while len(message) % 2 != 0:
-                message += b'\x00'
+            audio_data = io.BytesIO(message)
+
+            # while len(message) % 2 != 0:
+            #     message += b'\x00'
                 
-            try:
-                audio_data = np.frombuffer(message, dtype=np.int16)
-                await websocket.send_text(f"2.completed np.frombuffer: {len(audio_data)}")
-            except Exception as e:
-                await websocket.send_text(f"2. Error np.frombuffer: {e}")
-                break
+            # try:
+            #     audio_data = np.frombuffer(message, dtype=np.int16).flatten()
+            #     await websocket.send_text(f"2.completed np.frombuffer: {len(audio_data)}")
+            # except Exception as e:
+            #     await websocket.send_text(f"2. Error np.frombuffer: {e}")
+            #     break
+
+            # try:
+            #     audio_data_float = audio_data.astype(np.float32) / 32768.0
+            #     await websocket.send_text(f"3. completed audio_data.astype: {len(audio_data_float)}")
+            # except Exception as e:
+            #     await websocket.send_text(f"3. Error audio_data.astype: {e}")
+            #     break
 
             try:
-                audio_data_float = audio_data.astype(np.float32) / 32768.0
-                await websocket.send_text(f"3. completed audio_data.astype: {len(audio_data_float)}")
-            except Exception as e:
-                await websocket.send_text(f"3. Error audio_data.astype: {e}")
-                break
-
-            try:
-                segments, info = get_transcription(audio_data_float)
-                await websocket.send_text(f"4. completed get_transcription: {list(segments)}")
-                for segment in segments:
-                    await websocket.send_text(f"{segment.start}, {segment.end}, {segment.text}")
+                if audio_data.getbuffer().nbytes > 0:
+                    segments, info = get_transcription(audio_data)
+                    await websocket.send_text(f"4. completed get_transcription: {list(segments)}")
+                    for segment in segments:
+                        await websocket.send_text(f"{segment.start}, {segment.end}, {segment.text}")
             except Exception as e:
                 await websocket.send_text(f"4. Error get_transcription: {e}")
-                break
+                # break
     except Exception as e:
         print(f"Error: {e}")
     finally:
