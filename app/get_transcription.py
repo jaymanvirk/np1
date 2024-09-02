df6be7c411830fae9dfe002f0a6eaf28e413f3ea
jaymanvirk <jay.manvirk@gmail.com>
2024-09-02 14:14:27 +0300

    moved model initiaion outside function to reduce time of each function call

--- app/get_transcription.py
+++ app/get_transcription.py
@@ -1,16 +1,13 @@
 from faster_whisper import WhisperModel
 
-def get_transcription(audio_data_float):
-    model_size = "tiny.en"
-    device = "cpu"
-    compute_type = "int8"
-    beam_size = 1
+
+model_size = "tiny.en"
+device = "cpu"
+compute_type = "int8"
+beam_size = 1
+model = WhisperModel(model_size, device=device, compute_type=compute_type)
+
+
+def get_transcription(audio_data):
     
-    # # Run on GPU with FP16
-    # model = WhisperModel(model_size, device="cuda", compute_type="float16")
-    # # or run on GPU with INT8
-    # model = WhisperModel(model_size, device="cuda", compute_type="int8_float16")
-    # or run on CPU with INT8
-    model = WhisperModel(model_size, device=device, compute_type=compute_type)
-    
-    return model.transcribe("test.m4a", beam_size=beam_size)
+    return model.transcribe(audio_data, beam_size=beam_size)
