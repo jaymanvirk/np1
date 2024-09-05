446c3db11b16ce0acc188e4b5d7884e99b7a0fd8
jaymanvirk <jay.manvirk@gmail.com>
2024-09-05 22:51:08 +0300

    added async to function

--- app/get_transcription.py
+++ app/get_transcription.py
@@ -8,6 +8,6 @@ beam_size = 1
 model = WhisperModel(model_size, device=device, compute_type=compute_type)
 
 
-def get_transcription(audio_data):
+async def get_transcription(audio_data):
     
     return model.transcribe(audio_data, beam_size=beam_size)
