489fcaf0cc0faa0e460957e47a2bce1ba856e883
jaymanvirk <jay.manvirk@gmail.com>
2024-09-06 16:34:25 +0300

    removed all but chat_log

--- app/html_content/get_main_html.py
+++ app/html_content/get_main_html.py
@@ -1,16 +1,5 @@
 def get_main_html(data):
 	html = '''
-		  <div id="drop_image">
-		    <p>Drag and drop image here or</p>
-		    <input type="file" id="select_image" accept="image/*">
-		  </div>
-
-		  <img id="preview_image" src="#" alt="Image Preview" style="max-width: 400px;">
-
-		  <button id="stream_audio">
-		  	Stream Audio
-		  </button>
-
 		  <div id="chat_log">
 		  </div>
     		'''
