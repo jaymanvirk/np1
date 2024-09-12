3dd933db885be18e50efc400954b5ef2c1170c88
jaymanvirk <jay.manvirk@gmail.com>
2024-09-12 13:43:26 +0300

    switched to cuda with beam size 5

--- app/get_transcription.py
+++ app/get_transcription.py
@@ -1,10 +1,10 @@
 from faster_whisper import WhisperModel
 
 
-model_size = "tiny.en"
-device = "cpu"
+model_size = "tiny"
+device = "cuda"
 compute_type = "int8"
-beam_size = 1
+beam_size = 5
 model = WhisperModel(model_size, device=device, compute_type=compute_type)
 
 
