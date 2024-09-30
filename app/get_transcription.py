9acc16745a612f5aede12a7e56eff0e163d2bfca
jaymanvirk <jay.manvirk@gmail.com>
2024-09-30 17:16:05 +0300

    added environment vars, removed hardcoded

--- app/get_transcription.py
+++ app/get_transcription.py
@@ -1,10 +1,10 @@
 from faster_whisper import WhisperModel
+import os
 
-
-model_size = "tiny"
-device = "cuda"
-compute_type = "int8"
-beam_size = 5
+model_size = os.getenv("WHISPER_MODEL_SIZE")
+device = os.getenv("WHISPER_DEVICE")
+compute_type = os.getenv("WHISPER_COMPUTE_TYPE")
+beam_size = int(os.getenv("WHISPER_BEAM_SIZE"))
 model = WhisperModel(model_size, device=device, compute_type=compute_type)
 
 
