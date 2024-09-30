5d05b66ecd7532feab431d03715208b9294a8b5c
jaymanvirk <jay.manvirk@gmail.com>
2024-09-30 17:15:20 +0300

    added import and played with default value of ms

--- app/get_processed_audio.py
+++ app/get_processed_audio.py
@@ -1,7 +1,10 @@
-def get_processed_audio(audio_bytes, ms = 100):
+from pydub import AudioSegment
+import io
+
+def get_processed_audio(audio_bytes, ms = 500):
     audio_segment = AudioSegment.from_file(io.BytesIO(audio_bytes))
     audio_data = audio_segment[-ms:]
-    byte_stream = io.BytesIO()
+    byte_stream = io.BytesIO(audio_bytes)
     audio_data.export(byte_stream, format="wav")
     byte_stream.seek(0)
 
