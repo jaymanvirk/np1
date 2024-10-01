d5e39b5447421e43393e0830c5d622a6730aeff8
jaymanvirk <jay.manvirk@gmail.com>
2024-10-01 10:01:59 +0300

    replaced arbitrary number of ms with half the audio length

--- app/get_processed_audio.py
+++ app/get_processed_audio.py
@@ -1,9 +1,10 @@
 from pydub import AudioSegment
 import io
 
-def get_processed_audio(audio_bytes, ms = 500):
+def get_processed_audio(audio_bytes):
     audio_segment = AudioSegment.from_file(io.BytesIO(audio_bytes))
-    audio_data = audio_segment[-ms:]
+    indx = len(audio_segment)//2
+    audio_data = audio_segment[-indx:]
     byte_stream = io.BytesIO(audio_bytes)
     audio_data.export(byte_stream, format="wav")
     byte_stream.seek(0)
