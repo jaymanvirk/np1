554b0810a7c9773b25da74aa8e0cd72673a3faf2
jaymanvirk <jay.manvirk@gmail.com>
2024-10-01 13:44:36 +0300

    added audio receiver to auto play

--- static/js/stream_audio.js
+++ static/js/stream_audio.js
@@ -1,13 +1,27 @@
 async function stream_audio(){
 	const ws = new WebSocket("ws://localhost:8000/stream/v1/audio");
+	//ws.binaryType = "arraybuffer";
 	const transcriptionDiv = document.getElementById('chat_log');
+	const audioElement = document.getElementById('audio');
+
+	const audioContext = new (window.AudioContext || window.webkitAudioContext)();
 
 	ws.onopen = async () => {
 		console.log("WebSocket connection established");
 	};
 
-	ws.onmessage = (event) => {
-	    transcriptionDiv.innerHTML += event.data + "<br>";
+	ws.onmessage = async (event) => {
+		try {
+		    const data = event.data;
+	    	//transcriptionDiv.innerHTML += data + "<br>";
+	    	const buffer = await audioContext.decodeAudioData(data);
+	        const source = audioContext.createBufferSource();
+	        source.buffer = buffer;
+	        source.connect(audioContext.destination);
+	        source.start(0);
+        } catch (error) {
+            console.error("Error decoding audio data", error);
+        };
 	};
 
 	ws.onclose = () => {
