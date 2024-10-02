072202ff4586f17640ff4981407760a99c1709d6
jaymanvirk <jay.manvirk@gmail.com>
2024-10-02 14:37:02 +0300

    removed indexing and return full audio stream

--- app/get_processed_audio.py
+++ app/get_processed_audio.py
@@ -3,9 +3,8 @@ import io
 
 def get_processed_audio(audio_bytes):
     audio_segment = AudioSegment.from_file(io.BytesIO(audio_bytes))
-    indx = len(audio_segment)//2
     byte_stream = io.BytesIO()
-    audio_segment[indx:].export(byte_stream, format="wav")
+    audio_segment.export(byte_stream, format="wav")
     byte_stream.seek(0)
 
     return byte_stream
