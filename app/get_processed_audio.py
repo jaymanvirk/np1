0eaf1076287764f4ae975165b02b66021f941c09
jaymanvirk <jay.manvirk@gmail.com>
2024-10-03 19:04:23 +0300

    added argument ms to use it as index to cut audio_chunk_0 from byte_stream

--- app/get_processed_audio.py
+++ app/get_processed_audio.py
@@ -1,10 +1,10 @@
 from pydub import AudioSegment
 import io
 
-def get_processed_audio(audio_bytes):
+async def get_processed_audio(audio_bytes, ms = 500):
     audio_segment = AudioSegment.from_file(io.BytesIO(audio_bytes))
     byte_stream = io.BytesIO()
-    audio_segment.export(byte_stream, format="wav")
+    audio_segment[ms:].export(byte_stream, format="wav")
     byte_stream.seek(0)
 
     return byte_stream
