3eeec3804b230578f675dc9504db0633f71c8a4a
jaymanvirk <jay.manvirk@gmail.com>
2024-10-03 19:06:40 +0300

    added logic of detecting when the thought complete or user silence

--- app/websocket_router.py
+++ app/websocket_router.py
@@ -1,8 +1,8 @@
 from fastapi import WebSocket, WebSocketDisconnect, APIRouter
 from get_transcription import get_transcription
 from get_processed_audio import get_processed_audio
-import asyncio
-from concurrent.futures import ThreadPoolExecutor
+# import asyncio
+# from concurrent.futures import ThreadPoolExecutor
 import time
 import io
 
@@ -27,28 +27,39 @@ async def handle_upload_image(websocket: WebSocket):
 @router.websocket("/v1/audio")
 async def handle_stream_audio(websocket: WebSocket):
     await websocket.accept()
+    pt = ''
 
     try:
         audio_chunk_0 = await websocket.receive_bytes()
         combined_audio = audio_chunk_0
-        with ThreadPoolExecutor() as executor:
-            while True:
-                audio_chunk = await websocket.receive_bytes()
-                combined_audio += audio_chunk
 
-                st = time.time()
-                # byte_stream = await asyncio.get_event_loop().run_in_executor(executor, get_processed_audio, combined_audio)
-                byte_stream = await asyncio.get_event_loop().run_in_executor(executor, lambda: io.BytesIO(combined_audio))
-                # await websocket.send_bytes(byte_stream.getvalue())
-                
-                segments, _ = await get_transcription(byte_stream)
+        while True:
+            st = time.time()
+            audio_chunk = await websocket.receive_bytes()
+            combined_audio += audio_chunk
+            ln = len(combined_audio)
 
-                transcription = " ".join([segment.text for segment in segments])
+            byte_stream = await get_processed_audio(combined_audio)
+            # byte_stream = await asyncio.get_event_loop().run_in_executor(executor, lambda: io.BytesIO(combined_audio))
+            
+            segments, _ = await get_transcription(byte_stream)
 
-                if transcription.strip():
-                    t = time.time() - st
-                    ln = len(combined_audio)
-                    await websocket.send_text(f'time: {t:.3f} | length: {ln} | {transcription}')
+            transcription = " ".join([segment.text for segment in segments]).strip()
+            
+            if transcription:
+                if pt == transcription:
+                    # TODO: 
+                    # run a model "if the thought complete?"
+                    # if yes, send to a bigger model
+
+                    combined_audio = audio_chunk_0 + audio_chunk
+                else:
+                    pt = transcription
+                t = time.time() - st
+                await websocket.send_text(f'time: {t:.3f} | length: {ln} | {transcription}')
+
+            
+
 
 
     except Exception as e:
