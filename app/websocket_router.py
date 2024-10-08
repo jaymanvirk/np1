88cfd1f1cf10c80ee2132ba82ef0f660e986d1ef
jaymanvirk <jay.manvirk@gmail.com>
2024-10-08 16:21:06 +0300

    added AudioState shared class; added Queue; added process_queue function; added background_task

--- app/websocket_router.py
+++ app/websocket_router.py
@@ -1,12 +1,64 @@
 from fastapi import WebSocket, WebSocketDisconnect, APIRouter
 from get_transcription import get_transcription
 from get_processed_audio import get_processed_audio
-# import asyncio
-# from concurrent.futures import ThreadPoolExecutor
+import asyncio
 import time
-import io
+
 
 router = APIRouter()
+
+class AudioState:
+    def __init__(self):
+        self.audio_chunk_0 = b''
+        self.combined_audio = b''
+        self.prev_transcription = ""
+
+
+@router.websocket("/v1/audio")
+async def handle_stream_audio(websocket: WebSocket):
+    await websocket.accept()
+    queue = asyncio.Queue()
+    audio_state = AudioState()
+    asyncio.create_task(process_queue(websocket, queue, audio_state))
+
+    try:
+        audio_state.audio_chunk_0 = await websocket.receive_bytes()
+        audio_state.combined_audio = audio_state.audio_chunk_0
+
+        while True:
+            audio_chunk = await websocket.receive_bytes()
+            await queue.put(audio_chunk)
+
+    except Exception as e:
+        await websocket.send_text(f"Error: {e}")
+
+    finally:
+        await websocket.send_text(f"closing websocket")
+        await websocket.close()
+
+
+async def process_queue(websocket: WebSocket
+                        , queue: asyncio.Queue
+                        , audio_state: AudioState):
+    while True:
+        st = time.time()
+        audio_chunk = await queue.get()
+        audio_state.combined_audio += audio_chunk
+        ln = len(audio_state.combined_audio)
+
+        byte_stream = await get_processed_audio(audio_state.combined_audio)
+
+        transcription = await get_transcription(byte_stream)
+
+        if transcription:
+            if audio_state.prev_transcription == transcription:
+                audio_state.combined_audio = audio_state.audio_chunk_0 + audio_chunk
+            else:
+               audio_state.prev_transcription = transcription
+            t = time.time() - st
+
+            await websocket.send_text(f"time: {t:.3f} | length: {ln} | {transcription}")
+
 
 @router.websocket("/v1/image")
 async def handle_upload_image(websocket: WebSocket):
@@ -23,50 +75,6 @@ async def handle_upload_image(websocket: WebSocket):
 
     await websocket.send_text(f"Received {len(image_data)}")
 
-
-@router.websocket("/v1/audio")
-async def handle_stream_audio(websocket: WebSocket):
-    await websocket.accept()
-    pt = ''
-
-    try:
-        audio_chunk_0 = await websocket.receive_bytes()
-        combined_audio = audio_chunk_0
-
-        while True:
-            st = time.time()
-            audio_chunk = await websocket.receive_bytes()
-            combined_audio += audio_chunk
-            ln = len(combined_audio)
-
-            byte_stream = await get_processed_audio(combined_audio)
-            # byte_stream = await asyncio.get_event_loop().run_in_executor(executor, lambda: io.BytesIO(combined_audio))
-            
-            segments, _ = await get_transcription(byte_stream)
-
-            transcription = " ".join([segment.text for segment in segments]).strip()
-            
-            if transcription:
-                if pt == transcription:
-                    # TODO: 
-                    # run a model "if the thought complete?"
-                    # if yes, send to a bigger model
-
-                    combined_audio = audio_chunk_0 + audio_chunk
-                else:
-                    pt = transcription
-                t = time.time() - st
-                await websocket.send_text(f'time: {t:.3f} | length: {ln} | {transcription}')
-
-            
-
-
-
-    except Exception as e:
-        await websocket.send_text(f"Error: {e}")
-    finally:
-        await websocket.send_text(f"closing websocket")
-        await websocket.close()
 
 
 
