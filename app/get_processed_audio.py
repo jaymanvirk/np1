2e8c0018716c7a72191ec3d2df528a40b056c452
jaymanvirk <jay.manvirk@gmail.com>
2024-10-08 16:16:33 +0300

    refactored code

--- app/get_processed_audio.py
+++ app/get_processed_audio.py
@@ -1,6 +1,7 @@
 from pydub import AudioSegment
 import io
 
+
 async def get_processed_audio(audio_bytes, ms = 500):
     audio_segment = AudioSegment.from_file(io.BytesIO(audio_bytes))
     byte_stream = io.BytesIO()
