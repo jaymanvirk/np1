2a52f80f7794a9da57189e7fbfa5cf92d9ef0f53
jaymanvirk <jay.manvirk@gmail.com>
2024-10-15 15:46:33 +0300

    enabled auth_router

--- app/main.py
+++ app/main.py
@@ -7,7 +7,7 @@ from fastapi.middleware import Middleware
 
 app = FastAPI() # FastAPI(middleware=[Middleware(SanitizeMiddleware)])
 
-#app.include_router(auth_router, prefix="/auth")
+app.include_router(auth_router, prefix="/auth")
 app.include_router(ws_router, prefix="/stream")
 
 app.mount("/", StaticFiles(directory="static", html = True), name="static")
