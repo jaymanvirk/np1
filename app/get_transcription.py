809a3c872a10a2640440c45c92b092f9fc288e54
jaymanvirk <jay.manvirk@gmail.com>
2024-10-17 18:16:59 +0300

    removed load_model and pad_trim, since the function already receives cleaned audio bytes

--- app/get_transcription.py
+++ app/get_transcription.py
@@ -6,10 +6,7 @@ model_checkpoint = os.getenv("WHISPER_MODEL_CHECKPOINT")
 device = os.getenv("WHISPER_DEVICE")
 model = whisper.load_model(model_checkpoint).to(device) 
 
-async def get_transcription(byte_stream) -> str:
-    audio = whisper.load_audio(byte_stream)
-    audio = whisper.pad_or_trim(audio)
-
-    result = model.transcribe(audio)
+async def get_transcription(audio_data) -> str:
+    result = model.transcribe(audio_data)
 
     return result["text"]
