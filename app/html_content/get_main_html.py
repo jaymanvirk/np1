fc0c8863e3136b984913556800f5b2d4297e5435
jaymanvirk <jay.manvirk@gmail.com>
2024-10-21 12:01:01 +0300

    added top section; refactored code

--- app/html_content/get_main_html.py
+++ app/html_content/get_main_html.py
@@ -1,8 +1,49 @@
 def get_main_html(data):
-	html = '''
-		  <div id="chat_log">
-		  </div>
-		  <audio id="audio" style="display:none" controls autoplay></audio>
-    		'''
+    logomark = '''
+             <div id="logomark">
+                <table>
+                    <tr>
+                        <td class="bg-black"></td>
+                        <td class="bg-white"></td>
+                        <td class="bg-black"></td>
+                    </tr>
+                    <tr>
+                        <td class="bg-black"></td>
+                        <td class="bg-black"></td>
+                        <td class="bg-white"></td>
+                    </tr>
+                    <tr>
+                        <td class="bg-black"></td>
+                        <td class="bg-white"></td>
+                        <td class="bg-black"></td>
+                    </tr>
+                </table>
+            </div>
+    '''
 
-	return html
+    logotype = '''
+            <div id="logotype">
+                kompjuut
+            </div>
+    '''
+
+    header = f'''
+         <div id="header" class="fixed top">
+           {logomark}
+           {logotype}
+       </div>
+    '''
+
+    main = '''
+        <div id="main">
+            <div id="chat_log">
+            </div>
+        </div>
+    '''
+
+    html = f'''
+       {header}
+       {main}
+    '''
+
+    return html
