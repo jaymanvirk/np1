35603258c55ee0ea5fdd64a74b9b3a0b0ac11931
jaymanvirk <jay.manvirk@gmail.com>
2024-10-22 14:29:14 +0300

    added get_message() to process json message

--- static/js/utils.js
+++ static/js/utils.js
@@ -1,3 +1,25 @@
 function scroll_to_bottom() {
     window.scrollTo(0, document.body.scrollHeight);
 }
+
+
+function get_message(data) {
+    data = JSON.parse(data)
+    html = `
+        <div class="message">
+            <div class="sender display-ib">
+                <span class="name">
+                    ${data.sender.name}
+                </span>
+                <span class="colon">
+                    :
+                </span>
+            </div>
+            <div class="media-content display-ib">
+                ${data.media.text}
+            </div>
+        </div>
+        `
+
+    return html
+}
