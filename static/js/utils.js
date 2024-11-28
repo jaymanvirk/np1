bf4c5367a4e90e92cddbd6869769eae3191a1711
jaymanvirk <jay.manvirk@gmail.com>
2024-11-28 15:50:08 +0900

    updated get_message function to return html template for the message

--- static/js/utils.js
+++ static/js/utils.js
@@ -3,25 +3,24 @@ function scroll_to_bottom() {
 }
 
 
-function get_message(data) {
-    //TODO:
-    //check for the flag "thought complete" to add new message or update existing one
-    data = JSON.parse(data)
+function get_html_message() {
     html = `
         <div class="message">
             <div class="sender display-ib">
                 <span class="name">
-                    ${data.sender.name}
                 </span>
                 <span class="colon">
                     :
                 </span>
             </div>
             <div class="media-content display-ib">
-                ${data.media.text}
             </div>
         </div>
         `
 
     return html
+}
+
+function process_message(data){
+    data = JSON.parse(data)
 }
