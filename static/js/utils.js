f3c393a3cfe883e4e8e4fd6a69001f7d6ac3ea14
jaymanvirk <jay.manvirk@gmail.com>
2024-11-29 16:52:37 +0900

    updated code allocate messages in divs according to their ids

--- static/js/utils.js
+++ static/js/utils.js
@@ -2,25 +2,28 @@ function scroll_to_bottom() {
     window.scrollTo(0, document.body.scrollHeight);
 }
 
-
-function get_html_message() {
-    html = `
-        <div class="message">
+function get_html_message(data) {
+    return `
+        <div class="message" id="${data.meta.id}">
             <div class="sender display-ib">
-                <span class="name">
-                </span>
-                <span class="colon">
-                    :
-                </span>
+                <span class="name">${data.sender.name}</span>
+                <span class="colon">:</span>
             </div>
-            <div class="media-content display-ib">
-            </div>
+            <div class="media-content display-ib"></div>
         </div>
-        `
-
-    return html
+    `;
 }
 
-function process_message(data){
-    data = JSON.parse(data)
+function process_message(raw_data) {
+    const data = JSON.parse(raw_data);
+    const chat = document.getElementById('chat');
+    const m_div = document.querySelector(`div.message[id='${data.meta.id}']`);
+    
+    if (!m_div) {
+        chat.insertAdjacentHTML('beforeend', get_html_message(data));
+    }
+    
+    const m_content = document.querySelector(`div.message[id='${data.meta.id}'] > .media-content`);
+    m_content.textContent = data.media.text;
 }
+
