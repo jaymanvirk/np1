5b8178362c94d9918a9b4c88c537e67112698ef5
jaymanvirk <jay.manvirk@gmail.com>
2024-11-29 11:43:16 +0900

    added get_transcription function

--- app/stt_utils.py
+++ app/stt_utils.py
@@ -2,6 +2,18 @@ import io
 import numpy as np
 import soundfile as sf
 import subprocess
+import whisper
+import os
+
+
+model_checkpoint = os.getenv("WHISPER_MODEL_CHECKPOINT")
+device = os.getenv("WHISPER_DEVICE")
+model = whisper.load_model(model_checkpoint).to(device) 
+
+async def get_transcription(audio_data) -> str:
+    result = model.transcribe(audio_data)
+
+    return result["text"]
 
 
 async def get_processed_audio(audio_bytes, ms = 500):
