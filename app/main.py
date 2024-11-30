5aefbb319cfd83dc7ca1d2be8df3fec87300a2fd
jaymanvirk <jay.manvirk@gmail.com>
2024-11-30 22:21:49 +0900

    removed not working sanitize middleware

--- app/main.py
+++ app/main.py
@@ -3,9 +3,8 @@ from fastapi.staticfiles import StaticFiles
 from auth_router import router as auth_router
 from websocket_router import router as ws_router
 from fastapi.middleware import Middleware
-# from sanitize_middleware import SanitizeMiddleware
 
-app = FastAPI() # FastAPI(middleware=[Middleware(SanitizeMiddleware)])
+app = FastAPI() 
 
 app.include_router(auth_router, prefix="/auth")
 app.include_router(ws_router, prefix="/stream")
