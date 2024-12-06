20ecd18e94460cac8224b5792cc669aa351defc1
jaymanvirk <jay.manvirk@gmail.com>
2024-12-06 22:01:55 +0900

    removed GPU from silero-vad since it's not supported

--- app/stt_utils.py
+++ app/stt_utils.py
@@ -8,9 +8,8 @@ import torch
 
 
 VAD_MODEL, VAD_UTILS = torch.hub.load(repo_or_dir='snakers4/silero-vad', model='silero_vad', onnx=True)
-VAD_MODEL = VAD_MODEL.cuda()
 MAX_AUDIO_LENGTH = 16000
-AUDIO_TENSOR = torch.zeros(1, MAX_AUDIO_LENGTH, device='cuda', dtype=torch.float32)
+AUDIO_TENSOR = torch.zeros(1, MAX_AUDIO_LENGTH, dtype=torch.float32)
 
 
 STT_CHECKPOINT = os.getenv("STT_CHECKPOINT")
@@ -28,10 +27,10 @@ async def is_speech(processed_audio, threshold=0.5, sampling_rate=16000):
     # Ensure audio is in the correct format and copy to pre-allocated tensor
     audio_length = min(len(processed_audio), MAX_AUDIO_LENGTH) 
     with torch.cuda.stream(torch.cuda.Stream()):
-        audio_tensor[0, :audio_length].copy_(torch.from_numpy(processed_audio[:audio_length]), non_blocking=True)
+        AUDIO_TENSOR[0, :audio_length].copy_(torch.from_numpy(processed_audio[:audio_length]), non_blocking=True)
     
     # Run VAD asynchronously
-    speech_prob = await asyncio.to_thread(lambda: model(audio_tensor[:, :audio_length], sampling_rate).item())
+    speech_prob = await asyncio.to_thread(lambda: VAD_MODEL(AUDIO_TENSOR[:, :audio_length], sampling_rate).item())
     
     return speech_prob > threshold
 
