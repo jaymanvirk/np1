a71486640d07cc7bde968bd307c9826d16afb732
jaymanvirk <jay.manvirk@gmail.com>
2024-12-12 17:38:29 +0900

    added half-precision floating point format for faster inference; disabled gradient calculation to reduce memory usage and speed up computation

--- app/stt_utils.py
+++ app/stt_utils.py
@@ -1,14 +1,13 @@
 import whisper
 import os
-
+import torch
 
 STT_CHECKPOINT = os.getenv("STT_CHECKPOINT")
 GPU_DEVICE = os.getenv("GPU_DEVICE")
-STT_MODEL = whisper.load_model(STT_CHECKPOINT).to(GPU_DEVICE) 
-
+STT_MODEL = whisper.load_model(STT_CHECKPOINT).to(GPU_DEVICE).half()
 
 async def get_transcription(audio_data) -> str:
-    result = STT_MODEL.transcribe(audio_data)
-
+    with torch.no_grad():
+        result = STT_MODEL.transcribe(audio_data)
+    
     return result["text"]
-
