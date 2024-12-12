a46884c3511528f2b4d8edde83b892cfff9d42d8
jaymanvirk <jay.manvirk@gmail.com>
2024-12-13 00:59:41 +0900

    renamed prev_transcription to transcription

--- app/stt_manager.py
+++ app/stt_manager.py
@@ -5,7 +5,7 @@ class AudioState:
     def __init__(self):
         self.audio_chunk_0 = b''
         self.combined_audio = b''
-        self.prev_transcription = ""
+        self.transcription = ""
         # Lock for synchronizing access to shared state
         self.lock = asyncio.Lock()  
         self.id = 0
