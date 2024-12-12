daeb9ecdf61e97da0b5ce8630835113db4bdc2bf
jaymanvirk <jay.manvirk@gmail.com>
2024-12-12 21:31:45 +0900

    switched to generator use to avoid waiting for each await call to complete reducing latency

--- app/llm_utils.py
+++ app/llm_utils.py
@@ -12,21 +12,17 @@ async def stream_ollama_output(websocket, model_name: str, str_input: str):
     """Function to stream output from the Ollama subprocess."""
     await send_input_to_ollama(model_name, str_input)
     m_id = int(time.time())
-    while True:
-        response = await ollama_manager.get_output(model_name)
-        if response:
-            message = {
-                "sender": {
-                    "name": "K"
-                },
-                "meta": {
-                    "id": m_id
-                },
-                "media": {
-                    "text": response
-                }
+    async for output in ollama_manager.get_output(model_name):
+        message = {
+            "sender": {
+                "name": "K"
+            },
+            "meta": {
+                "id": m_id
+            },
+            "media": {
+                "text": output 
             }
-            await websocket.send_text(json.dumps(message))
-        else:
-            break
+        }
+        await websocket.send_text(json.dumps(message))
 
