ef6dcf91d0de6c96fe92e0016898f5d550d18dbf
jaymanvirk <jay.manvirk@gmail.com>
2024-12-13 17:13:50 +0900

    added sent_to_llm condition; changed argument to stt_manager class in stream_llm_output; renamed stream_ollama_output to stream_llm_output for general use

--- app/queue_utils.py
+++ app/queue_utils.py
@@ -1,6 +1,6 @@
 from audio_utils import get_processed_audio
-from stt_utils import get_transcription
-from llm_utils import stream_ollama_output
+from stt_utils import get_transcription 
+from llm_utils import stream_llm_output
 from vad_utils import is_speech
 import os
 import json
@@ -20,7 +20,8 @@ async def process_queue(websocket
 
         if speech:
             # Ensure exclusive access to shared state
-            async with stt_manager.lock:  
+            async with stt_manager.lock:
+                stt_manager.sent_to_llm = False
                 stt_manager.combined_audio += audio_chunk
         
                 audio_data = await get_processed_audio(stt_manager.combined_audio)
@@ -41,6 +42,8 @@ async def process_queue(websocket
                 }
 
             await websocket.send_text(json.dumps(message))
-        elif len(stt_manager.transcription):
-            await stream_ollama_output(websocket, LLM_CHECKPOINT, stt_manager.transcription)
+        elif not stt_manager.sent_to_llm:
+            async with stt_manager.lock:
+                stt_manager.sent_to_llm = True
+            await stream_llm_output(websocket, LLM_CHECKPOINT, stt_manager)
    
