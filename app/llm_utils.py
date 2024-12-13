ee3c749933cc54e389280bf8a130baddfd533c5e
jaymanvirk <jay.manvirk@gmail.com>
2024-12-13 17:33:05 +0900

    changed argument str_input to stt_manager class to be able to update it's values; added flag boolean to check if stt_manager needs to be updated based on the llm output

--- app/llm_utils.py
+++ app/llm_utils.py
@@ -8,11 +8,13 @@ async def send_input_to_ollama(model_name: str, str_input: str):
     await ollama_manager.start_process(model_name)
     await ollama_manager.send_input(model_name, str_input)
 
-async def stream_ollama_output(websocket, model_name: str, str_input: str):
+async def stream_llm_output(websocket, model_name: str, stt_manager):
     """Function to stream output from the Ollama subprocess."""
-    await send_input_to_ollama(model_name, str_input)
+    await send_input_to_ollama(model_name, stt_manager.transcription)
     m_id = int(time.time())
+    flag = False
     async for output in ollama_manager.get_output(model_name):
+        flag = True
         message = {
             "sender": {
                 "name": "K"
@@ -25,4 +27,8 @@ async def stream_ollama_output(websocket, model_name: str, str_input: str):
             }
         }
         await websocket.send_text(json.dumps(message))
-
+    
+    if flag:
+        async with stt_manager.lock:
+            stt_manager.id += 1
+            stt_manager.combined_audio = stt_manager.audio_chunk_0
