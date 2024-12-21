52991d4100ce14852568a223b4be7d68a77fd61c
jaymanvirk <jay.manvirk@gmail.com>
2024-12-22 00:29:13 +0900

    added sentence split to generate speech sentence by sentence

--- app/llm_utils.py
+++ app/llm_utils.py
@@ -3,13 +3,19 @@ from llm_manager import ollama_manager
 from tts_manager import tts_manager
 import time
 import json
+import re
 
+async def stream_audio(websocket, text):
+    audio_bytes = await tts_manager.get_output(text)
+    await websocket.send_bytes(audio_bytes)
 
 async def stream_llm_output(websocket, model_name: str, stt_manager):
     """Function to stream output from the Ollama subprocess."""
     m_id = int(time.time())
     flag = False
     text = ""
+    sentence_end = re.compile(r'(?<!\w\.\w.)(?<![A-Z][a-z]\.)(?<=\.|\?|!)\s')
+    incomplete_sentence = ""
     async for output in ollama_manager.chat(stt_manager.transcription):
         flag = True
         text += output
@@ -24,11 +30,19 @@ async def stream_llm_output(websocket, model_name: str, stt_manager):
                 "text": text 
             }
         }
+        tmp = incomplete_sentence + output + " "
+        sentences = sentence_end.split(tmp)
+        if len(sentences) > 1:
+            incomplete_sentence = sentences[-1]
+            await stream_audio(websocket, sentences[0])
+        else:
+            incomplete_sentence += output + " "
+
         await websocket.send_text(json.dumps(message))
 
-    audio_bytes = await tts_manager.get_output(text)
-    await websocket.send_bytes(audio_bytes)
-    
+    if incomplete_sentence:
+        await stream_audio(websocket, incomplete_sentence)
+
     if flag:
         async with stt_manager.lock:
             stt_manager.id += 1
