0e1c1a5eb189b630c28df3548803f56ae45fddd4
jaymanvirk <jay.manvirk@gmail.com>
2024-12-25 12:31:37 +0900

    added ollama_manager initialization and as an argument to process_queue

--- app/websocket_router.py
+++ app/websocket_router.py
@@ -1,8 +1,12 @@
 from fastapi import WebSocket, APIRouter
 from queue_utils import process_queue
 from stt_manager import STTManager 
-from llm_manager import ollama_manager
+from llm_manager import OllamaManager 
 import asyncio
+import os
+
+OLLAMA_URL = os.getenv("OLLAMA_URL")
+LLM_MODEL_NAME = os.getenv("LLM_MODEL_NAME")
 
 router = APIRouter()
 
@@ -11,10 +15,11 @@ async def handle_stream_audio(websocket: WebSocket):
     await websocket.accept()
     queue = asyncio.Queue()
     stt_manager = STTManager()
-    
+   
+    ollama_manager = OllamaManager(OLLAMA_URL, LLM_MODEL_NAME)
     await ollama_manager.start_session()
 
-    stream_task = asyncio.create_task(process_queue(websocket, queue, stt_manager))
+    stream_task = asyncio.create_task(process_queue(websocket, queue, stt_manager, ollama_manager))
 
     try:
         stt_manager.audio_chunk_0 = await websocket.receive_bytes()
