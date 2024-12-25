d5b9ade6211b8a8dc3bcd274bda65a42c47e9c7b
jaymanvirk <jay.manvirk@gmail.com>
2024-12-25 15:46:34 +0900

    replaced audio control function and vars with AudioManager class; replaced process message and scroll to bottom with process_data function

--- static/js/stream_audio.js
+++ static/js/stream_audio.js
@@ -2,57 +2,7 @@ async function stream_audio(){
     const ws = new WebSocket("wss://kompjuut.com/stream/v1/audio");
     ws.binaryType = "arraybuffer";
 
-    const audio_context = new (window.AudioContext || window.webkitAudioContext)();
-    let audio_queue = [];
-    let is_playing = false;
-    function play_next_queue() {
-        if (audio_queue.length === 0) {
-            is_playing = false;
-            return;
-        }
-
-        is_playing = true;
-        const buffer = audio_queue.shift();
-        const source = audio_context.createBufferSource();
-        source.buffer = buffer;
-        source.playbackRate.value = 1.25;
-        source.connect(audio_context.destination);
-
-        source.onended = play_next_queue;
-        source.start(0);
-    }
-    ws.onopen = async () => {
-        console.log("WebSocket connection established");
-    };
-
-    ws.onmessage = async (event) => {
-        try {
-            const data = event.data;
-            if (typeof data === 'string'){
-                process_message(data);
-                scroll_to_bottom();
-            } else {
-                const buffer = await audio_context.decodeAudioData(data);
-                audio_queue.push(buffer);
-                if (!is_playing) {
-                    play_next_queue();
-                }
- 
-            }
-        } catch (error) {
-            console.error("Error decoding audio data", error);
-        };
-    };
-
-    ws.onclose = () => {
-        console.log('WebSocket connection closed');
-        mediaRecorder.stop();
-    };
-
-    ws.onerror = (event) => {
-        console.log('WebSocket error:', event);
-        mediaRecorder.stop();
-    };
+    const audio_manager = new AudioManager()
 
     let mediaRecorder;
     navigator.mediaDevices.getUserMedia({ audio: true })
@@ -68,5 +18,32 @@ async function stream_audio(){
         mediaRecorder.start(500);
     })
     .catch(error => console.error("Error accessing microphone: ", error));
+
+    ws.onopen = async () => {
+        console.log("WebSocket connection established");
+    };
+
+    ws.onmessage = async (event) => {
+        try {
+            const data = event.data;
+            if (typeof data === 'string'){
+                process_data(data);
+            } else {
+                audio_manager.play_audio(data);
+            }
+        } catch (error) {
+            console.error("Error decoding audio data", error);
+        };
+    };
+
+    ws.onclose = () => {
+        console.log('WebSocket connection closed');
+        mediaRecorder.stop();
+    };
+
+    ws.onerror = (event) => {
+        console.error('WebSocket error:', event);
+        mediaRecorder.stop();
+    };
 };
 
