11cd26baab57394798be1c57c1d5a35af41c6871
jaymanvirk <jay.manvirk@gmail.com>
2024-12-26 16:42:27 +0900

    added async method; added current_source internal variable; removed .suspend() .resume() to simplify audio stop

--- static/js/audio_manager.js
+++ static/js/audio_manager.js
@@ -5,9 +5,10 @@ class AudioManager {
         this.is_playing = false;
         this.playback_rate = playback_rate;
         this.playback_volume = playback_volume;
+        this.current_source = null;
     }
 
-    play_next_queue() {
+    async play_next_queue() {
         if (this.audio_queue.length === 0) {
             this.is_playing = false;
             return;
@@ -15,32 +16,37 @@ class AudioManager {
 
         this.is_playing = true;
         const buffer = this.audio_queue.shift();
-        const source = this.audio_context.createBufferSource();
-        source.buffer = buffer;
-        source.playbackRate.value = this.playback_rate;
-        source.connect(this.audio_context.destination);
+        this.current_source = this.audio_context.createBufferSource();
+        this.current_source.buffer = buffer;
+        this.current_source.playbackRate.value = this.playback_rate;
+        this.current_source.connect(this.audio_context.destination);
 
-        source.onended = () => this.play_next_queue();
-        source.start(0);
+        this.current_source.onended = () => this.play_next_queue();
+        this.current_source.start(0);
     }
 
     async add_to_queue(data) {
-        const buffer = await this.audio_context.decodeAudioData(data);
-        this.audio_queue.push(buffer);
+        try {
+            const buffer = await this.audio_context.decodeAudioData(data);
+            this.audio_queue.push(buffer);
+        } catch (error) {
+            console.error('Error decoding audio data:', error);
+        }
     }
 
-    play_audio(data) {
+    async play_audio(data) {
         await this.add_to_queue(data);
         if (!this.is_playing) {
-            this.play_next_queue();
+            await this.play_next_queue();
         }
     }
 
     stop_audio() {
-        this.audio_context.suspend();
+        if (this.current_source) {
+            this.current_source.stop();
+        }
         this.audio_queue = [];
         this.is_playing = false;
-        this.audio_context.resume();
     }
 }
 
