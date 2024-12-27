0a9bdf9d4168e07b900dc22425b1de0187eedce7
jaymanvirk <jay.manvirk@gmail.com>
2024-12-27 16:15:50 +0900

    added condition on whether to get response from llm

--- app/stream_utils.py
+++ app/stream_utils.py
@@ -22,31 +22,35 @@ async def stream_output(websocket, model_name: str, stt_manager, llm_manager):
     incomplete_sentence = ""
     sentence_endings = r'(?<=[.!?])(?:\s+)?(?=[A-Z])'
     agen = llm_manager.chat(stt_manager.transcription)
-    async for output in agen:
-        tmp = incomplete_sentence + output + " "
-        sentences = re.split(sentence_endings, tmp) 
-        if len(sentences) > 1:
-            incomplete_sentence = sentences[-1]
-            await stream_audio(websocket, sentences[0])
-        else:
-            incomplete_sentence += output + " "
-        text += output
-        message = {
-            "type": "message"
-            ,"sender": {
-                "name": "K"
+    output = await anext(agen)
+    if "§" not in output:
+        text = output
+        incomplete_sentence = output
+        async for output in agen:
+            tmp = incomplete_sentence + output + " "
+            sentences = re.split(sentence_endings, tmp)
+            if len(sentences) > 1:
+                incomplete_sentence = sentences[-1]
+                await stream_audio(websocket, sentences[0])
+            else:
+                incomplete_sentence += output + " "
+            text += output
+            message = {
+                "type": "message"
+                ,"sender": {
+                    "name": "K"
+                }
+                ,"meta": {
+                    "id": m_id
+                }
+                ,"media": {
+                    "text": text
+                }
             }
-            ,"meta": {
-                "id": m_id
-            }
-            ,"media": {
-                "text": text
-            }
-        }
-        asyncio.create_task(websocket.send_text(json.dumps(message)))
+            asyncio.create_task(websocket.send_text(json.dumps(message)))
 
-    if incomplete_sentence:
-        await stream_audio(websocket, incomplete_sentence)
+        if incomplete_sentence:
+            await stream_audio(websocket, incomplete_sentence)
 
     if text:
         async with stt_manager.lock:
