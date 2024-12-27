9a16a30509992b45bf8f21e69630c05ea5d5e4ec
jaymanvirk <jay.manvirk@gmail.com>
2024-12-27 19:34:49 +0900

    added start_process and self.process to avoid spawning multiple processes

--- app/tts_manager.py
+++ app/tts_manager.py
@@ -4,18 +4,24 @@ import asyncio
 class TTSManager:
     def __init__(self, model_checkpoint):
         self.model_checkpoint = model_checkpoint
+        self.process = None
+
+    async def start_process(self, text):
+        if self.process is None:
+            cmd = ["piper-cli", "--model", self.model_checkpoint, "--output_file", "-", "--cuda"]
+
+            self.process = await asyncio.create_subprocess_exec(
+                    *cmd,
+                    stdin=asyncio.subprocess.PIPE,
+                    stdout=asyncio.subprocess.PIPE,
+                    stderr=asyncio.subprocess.PIPE
+            )
 
     async def get_output(self, text):
-        cmd = ["piper-cli", "--model", self.model_checkpoint, "--output_file", "-", "--cuda"]
+        if self.process is None:
+            await self.start_process()
 
-        process = await asyncio.create_subprocess_exec(
-                *cmd,
-                stdin=asyncio.subprocess.PIPE,
-                stdout=asyncio.subprocess.PIPE,
-                stderr=asyncio.subprocess.PIPE
-        )
-
-        stdout, stderr = await process.communicate(input=text.encode())
+        stdout, stderr = await self.process.communicate(input=text.encode())
 
         return stdout
 
