c93301ce847b393abad1dad0513c256f5fc1c588
jaymanvirk <jay.manvirk@gmail.com>
2024-12-28 11:44:35 +0900

    added jit script to optimized performance

--- app/stt_utils.py
+++ app/stt_utils.py
@@ -5,9 +5,11 @@ import torch
 STT_CHECKPOINT = os.getenv("STT_CHECKPOINT")
 GPU_DEVICE = os.getenv("GPU_DEVICE")
 STT_MODEL = whisper.load_model(STT_CHECKPOINT).to(GPU_DEVICE)
+STT_MODEL = torch.jit.script(STT_MODEL)
 
 async def get_transcription(audio_data) -> str:
     with torch.no_grad():
         result = STT_MODEL.transcribe(audio_data)
     
     return result["text"]
+
