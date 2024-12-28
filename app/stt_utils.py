6d678f6fb6a60d342a0e113805074fe6a57c6f5c
jaymanvirk <jay.manvirk@gmail.com>
2024-12-28 16:28:29 +0900

    updated model checkpoint path collection

--- app/stt_utils.py
+++ app/stt_utils.py
@@ -2,9 +2,11 @@ import whisper
 import os
 import torch
 
+MC_DIR = os.getenv("MC_DIR")
+STT_DIR = os.getenv("STT_DIR")
 STT_CHECKPOINT = os.getenv("STT_CHECKPOINT")
 GPU_DEVICE = os.getenv("GPU_DEVICE")
-STT_MODEL = whisper.load_model(STT_CHECKPOINT).to(GPU_DEVICE)
+STT_MODEL = whisper.load_model(f'{MC_DIR}/{STT_DIR}/{STT_CHECKPOINT}').to(GPU_DEVICE)
 STT_MODEL = torch.jit.script(STT_MODEL)
 
 async def get_transcription(audio_data) -> str:
