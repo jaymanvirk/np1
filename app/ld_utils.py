0bb859284178b73a99b4c5dc830579b02c5eac5b
jaymanvirk <jay.manvirk@gmail.com>
2025-01-01 21:28:04 +0900

    added lingua alongside pycld2

--- app/ld_utils.py
+++ app/ld_utils.py
@@ -1,5 +1,11 @@
 import pycld2 as cld2
+from lingua import Language, LanguageDetectorBuilder
 
 def detect_language(text):
-    _, _, _, language_bytes = cld2.detect(text, bestEffort=True, returnVectors=True)
+    _, _, _, vectors = cld2.detect(text, bestEffort=True, returnVectors=True)
+
+    languages = [Language.ENGLISH, Language.RUSSIAN]
+    detector = LanguageDetectorBuilder.from_languages(*languages).build()
+    lingua_result = detector.detect_multiple_languages_of(text)
+
     return language_bytes[0][1]
