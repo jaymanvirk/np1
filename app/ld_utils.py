0b734ae922b1156a6a92034d6d71656e1f930ee2
jaymanvirk <jay.manvirk@gmail.com>
2025-01-03 20:44:50 +0900

    added get_detected_langs_text function; moved back pycld2

--- app/ld_utils.py
+++ app/ld_utils.py
@@ -1,9 +1,69 @@
 from lingua import Language, LanguageDetectorBuilder
+import pycld2 as cld2
+from ld_manager import LangListManager
+import re
 
-def detect_language(text):
+LINGUA_LANGUAGES = os.getenv('LINGUA_LANGUAGES').split(',')
+LANGUAGES = [getattr(Language, lang.strip().upper()) for lang in LINGUA_LANGUAGES]
+DETECTOR = LanguageDetectorBuilder.from_languages(*LANGUAGES).build()
 
-    languages = [Language.ENGLISH, Language.RUSSIAN]
-    detector = LanguageDetectorBuilder.from_languages(*languages).build()
-    lingua_result = detector.detect_multiple_languages_of(text)
+def get_detected_langs_text(text):
+    result = DETECTOR.detect_multiple_languages_of(text)
+    langs_lingua = {}
+    for r in result:
+        langs_lingua.setdefault(r.language.name, []).append((r.start_index, r.end_index))
 
-    return lingua_result 
+    _, _, details, _ = cld2.detect(text, bestEffort=True, returnVectors=True)
+
+    langs_cld2 = set(d[0] for d in details if d[0] != 'Unknown')
+
+    llk = set(langs_lingua.keys())
+    d = langs_cld2 ^ llk
+    s = langs_cld2 & llk
+
+    if d:
+        if d & llk:
+            d = d & llk
+        else:
+            d = s
+            s = set()
+
+    prev_lang = ''
+    word_pos = {}
+    linked_list = LangListManager()
+
+    for l in s:
+        for st, en in langs_lingua[l]:
+            linked_list.insert(l, st, en)
+
+    for l in d:
+       for st, en in langs_lingua[l]:
+            for match in re.finditer(r'\S+', text[st:en]):
+                word = match.group()
+                start, end = match.span()
+                start += st
+                end += st
+
+                lw = DETECTOR.detect_multiple_languages_of(word)
+                if lw:
+                    lw = lw[0]
+                else:
+                    break
+                lang = lw.language.name
+                pos = [start, end]
+                if lang in word_pos:
+                    word_pos[lang][1] = pos[1]
+                else:
+                    if prev_lang in word_pos:
+                        tmp = word_pos[prev_lang]
+                        linked_list.insert(prev_lang, tmp[0], tmp[1])
+                        del word_pos[prev_lang]
+                    word_pos[lang] = pos
+                    prev_lang = lang
+
+    if prev_lang in word_pos:
+        tmp = word_pos[prev_lang]
+        linked_list.insert(prev_lang, tmp[0], tmp[1])
+
+
+    return linked_list.get_list_lang_text(text)
