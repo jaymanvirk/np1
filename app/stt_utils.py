f3a1561021fb3379af07e7c0fea70f8e246d6852
jaymanvirk <jay.manvirk@gmail.com>
2025-01-04 17:24:48 +0900

    removed jit.script due to error

--- app/stt_utils.py
+++ app/stt_utils.py
@@ -7,7 +7,6 @@ STT_DIR = os.getenv("STT_DIR")
 STT_CHECKPOINT = os.getenv("STT_CHECKPOINT")
 GPU_DEVICE = os.getenv("GPU_DEVICE")
 STT_MODEL = whisper.load_model(f'{MC_DIR}/{STT_DIR}/{STT_CHECKPOINT}').to(GPU_DEVICE)
-STT_MODEL = torch.jit.script(STT_MODEL)
 
 async def get_transcription(audio_data) -> str:
     with torch.no_grad():
