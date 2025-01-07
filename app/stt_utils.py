ea01ccb27de9dcdf2bcd4d50977fd83904e56368
jaymanvirk <jay.manvirk@gmail.com>
2025-01-07 16:31:14 +0900

    removed whisperx due to error with large-v3-turbo

--- app/stt_utils.py
+++ app/stt_utils.py
@@ -1,4 +1,4 @@
-import whisperx
+import whisper
 import os
 import torch
 
@@ -6,7 +6,7 @@ MC_DIR = os.getenv("MC_DIR")
 STT_DIR = os.getenv("STT_DIR")
 STT_CHECKPOINT = os.getenv("STT_CHECKPOINT")
 GPU_DEVICE = os.getenv("GPU_DEVICE")
-STT_MODEL = whisperx.load_model(f'{MC_DIR}/{STT_DIR}/{STT_CHECKPOINT}', device = GPU_DEVICE)
+STT_MODEL = whisper.load_model(f'{MC_DIR}/{STT_DIR}/{STT_CHECKPOINT}', device = GPU_DEVICE)
 
 async def get_transcription(audio_data) -> str:
     with torch.no_grad():
