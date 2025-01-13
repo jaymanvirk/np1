4aa7113e920c48da68c0b3cc45a2a878ea28739e
jaymanvirk <jay.manvirk@gmail.com>
2025-01-13 13:36:05 +0900

    added diff variable to avoid unprocessed bits of sentence

--- app/queue_utils.py
+++ app/queue_utils.py
@@ -36,18 +36,18 @@ async def process_queue(websocket
             async with stt_manager.lock:
                 stt_manager.sent_to_llm = False
                 stt_manager.audio_bytes += audio_chunk
-            if len(stt_manager.audio_bytes) >= (len(tasks_stt)+1)*8000:
+            diff = len(stt_manager.audio_bytes) - (len(tasks_stt)+1)*8000
+            if diff > -1:
                 tasks_stt.append(asyncio.create_task(stream_transcription(websocket, stt_manager)))
-
         elif not stt_manager.sent_to_llm:
             async with stt_manager.lock:
                 stt_manager.sent_to_llm = True
-
             for t in tasks_stt:
                 t.cancel()
             await asyncio.gather(*tasks_stt, return_exceptions=True)
             tasks_stt.clear()
-
+            if diff < 0:
+                await stream_transcription(websocket, stt_manager)
             tasks_llm.append(asyncio.create_task(stream_output(websocket, stt_manager, llm_manager, tts_manager)))
 
 
