5f204c42ba49bba7e282ce9552fb2f3539213b76
jaymanvirk <jay.manvirk@gmail.com>
2025-01-15 09:49:18 +0900

    added audio resampler to handle small audio samples; removed reduntan chunk_0 argument

--- app/audio_utils.py
+++ app/audio_utils.py
@@ -4,32 +4,23 @@ import soundfile as sf
 import asyncio 
 
 
-async def get_processed_audio(audio_chunk_0, audio_bytes):
-    combined_audio = audio_chunk_0 + audio_bytes
-
-    # Use ffmpeg to convert the input audio bytes to WAV
+async def get_processed_audio(audio_bytes):
     process = await asyncio.create_subprocess_exec(
-        'ffmpeg', '-i', 'pipe:0', '-f', 'wav', 'pipe:1',
+        'ffmpeg', '-i', 'pipe:0', '-af', 'aresample=async=1:min_hard_comp=0.100000:first_pts=0', '-f', 'wav', 'pipe:1',
         stdin=asyncio.subprocess.PIPE,
         stdout=asyncio.subprocess.PIPE,
         stderr=asyncio.subprocess.PIPE
     )
 
-    # Write the input bytes to ffmpeg's stdin and read from stdout
-    wav_data, _ = await process.communicate(input=combined_audio)
+    wav_data, _ = await process.communicate(input=audio_bytes)
 
-    # Read audio bytes directly into a numpy array using soundfile
     audio_segment, sr = await asyncio.to_thread(sf.read, io.BytesIO(wav_data))
 
-    # Calculate the number of samples to remove (length of audio_chunk_0)
     chunk_0_samples = int(len(audio_chunk_0) * sr / len(combined_audio))
 
-    # Remove audio_chunk_0 from audio_segment
     trimmed_audio = audio_segment[chunk_0_samples:]
 
-    # Resample to 16 kHz if necessary
     if sr != 16000:
-        # Calculate the resampling factor
         resample_factor = 16000 / sr
         new_length = int(len(trimmed_audio) * resample_factor)
         trimmed_audio = await asyncio.to_thread(np.interp,
