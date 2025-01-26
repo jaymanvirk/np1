5e43cd6e71c0b2cdfb63cc5ba94f24f40ec69fee
jaymanvirk <jay.manvirk@gmail.com>
2025-01-26 20:04:35 +0300

    lowered threshold to 0.5

--- app/vad_utils.py
+++ app/vad_utils.py
@@ -14,7 +14,7 @@ torch.set_num_interop_threads(1)
 CHUNK_SIZE = 512
 BATCH_SIZE = 10
 
-async def is_speech(processed_audio, threshold=0.63, sampling_rate=16000):
+async def is_speech(processed_audio, threshold=0.5, sampling_rate=16000):
     if len(processed_audio) % CHUNK_SIZE != 0:
         pad_length = CHUNK_SIZE - (len(processed_audio) % CHUNK_SIZE)
         processed_audio = np.pad(processed_audio, (0, pad_length), 'constant')
