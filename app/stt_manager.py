199441000ae600b9b30c35a28a6480c9b0227490
jaymanvirk <jay.manvirk@gmail.com>
2025-02-03 22:55:47 +0300

    removed temp transcription

--- app/stt_manager.py
+++ app/stt_manager.py
@@ -5,7 +5,7 @@ class STTManager:
     def __init__(self):
         self.audio_chunk_0 = b''
         self.audio_bytes = b''
-        self.transcription = "Hi"
+        self.transcription = ""
         # Lock for synchronizing access to shared state
         self.lock = asyncio.Lock()  
         self.sent_to_llm = True 
